951c5a4d24ebbe93d0dcc719c051f2d400c7f581
shoyamauchi <yonakiart@gmail.com>
2022-02-22 19:53:44 +0900

    third commit

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_091505) do
+ActiveRecord::Schema.define(version: 2022_02_22_104727) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "name"
